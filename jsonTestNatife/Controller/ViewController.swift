89febe096d32c1a16322f14dc4199fc4310147b1
Maroccostar <maroccostar@i.ua>
2023-09-05 14:55:31 +0300

    VievController update

--- jsonTestNatife/Controller/ViewController.swift
+++ jsonTestNatife/Controller/ViewController.swift
@@ -11,11 +11,35 @@ class ViewController: UIViewController {
 
     @IBOutlet private weak var tableView: UITableView!
     
-    let temporaryArray: [String] = ["0", "1", "2", "3"]
+    var viewModel: PostsViewModelType!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Title"
+        
         setupTableView()
+        
+        let filterButton = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(showFilterAlert))
+        navigationItem.rightBarButtonItem = filterButton
+        let resetButton = UIBarButtonItem(title: "Reset", style: .plain, target: self, action: #selector(resetFilter))
+        navigationItem.leftBarButtonItem = resetButton
+        
+        let displayMode: DisplayMode = navigationController?.tabBarItem.tag == 0 ? .feedsPosts : .specificPosts
+        
+        viewModel = PostsViewModel(with: displayMode)
+        viewModel.fetchData { [weak self] result in
+            switch result {
+            case.success(let posts):
+                self?.viewModel.allPosts = posts // +
+                self?.viewModel.filteredPosts = posts // +
+                self?.tableView.reloadData()
+            case.failure(let error):
+                debugPrint("Posts fetching error: \(error)")
+                self?.showError()
+            }
+        }
+        
     }
 }
 
@@ -26,13 +50,6 @@ private extension ViewController {
         tableView.delegate = self
         tableView.register(MyTableViewCell.nib(), forCellReuseIdentifier: MyTableViewCell.identifier)
     }
-    
-    func presentItemDetails(_ item: String) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "secondVC")
-        vc.navigationItem.title = item
-        navigationController?.pushViewController(vc, animated: true)
-    }
 }
 
 
@@ -40,22 +57,72 @@ private extension ViewController {
 // MARK: - UITableViewDataSource, UITableViewDelegate
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        presentItemDetails(temporaryArray[indexPath.row])
-
-    }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return temporaryArray.count
+        viewModel.filteredPosts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: MyTableViewCell.identifier, for: indexPath) as? MyTableViewCell else {
             return UITableViewCell()
         }
-        cell.textLabel?.text = temporaryArray[indexPath.row]
+        let post = viewModel.filteredPosts[indexPath.row]
+        cell.textLabel?.text = post.title
+        cell.detailTextLabel?.text = post.preview_text
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = viewModel.allPosts[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+
+    }
     
+}
+
+
+// MARK: - Private Methods: filter, reset, error
+private extension ViewController {
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func resetFilter() {
+        viewModel.filteredPosts = viewModel.allPosts
+        tableView.reloadData()
+    }
+    
+    func applyFilter(with keyword: String) {
+        if keyword.isEmpty {
+            resetFilter()
+            return
+        }
+        viewModel.filteredPosts = viewModel.allPosts.filter{ post in
+            return post.title.lowercased().contains(keyword.lowercased())
+        }
+        tableView.reloadData()
+    }
+    
+    @objc func showFilterAlert() {
+        let ac = UIAlertController(title: "Post filtering", message: "Enter a string to filter", preferredStyle: .alert)
+        ac.addTextField { textField in
+            textField.placeholder = "Enter keyword"
+            textField.textColor = .systemPink
+        }
+        
+        let filterAction = UIAlertAction(title: "Filter", style: .default) { [weak self, weak ac] _ in
+            if let keyword = ac?.textFields?.first?.text {
+                self?.applyFilter(with: keyword)
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        ac.addAction(filterAction)
+        ac.addAction(cancelAction)
+        present(ac, animated: true)
+    }
 }
